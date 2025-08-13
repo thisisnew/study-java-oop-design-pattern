ef3908e2f15b7ba7580a9afec1d8faedf1e09cc5
thisisnew <thisisnew@naver.com>
2025-08-13 21:27:23 +0900

    chap.12 팩토리 메서드 패턴

--- design-pattern/src/chapter12/factorymethod/ResponseTimeScheduler.java
+++ design-pattern/src/chapter12/factorymethod/ResponseTimeScheduler.java
@@ -1,8 +1,20 @@
 package chapter12.factorymethod;
 
 public class ResponseTimeScheduler implements ElevatorScheduler {
+
+    private static ElevatorScheduler scheduler;
+    private ResponseTimeScheduler() {}
+
+    public static ElevatorScheduler getInstance() {
+        if (scheduler == null) {
+            scheduler = new ResponseTimeScheduler();
+        }
+
+        return scheduler;
+    }
+
     @Override
     public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
-        return 0;
+        return 1;
     }
 }
--- design-pattern/src/chapter12/factorymethod/ThroughputScheduler.java
+++ design-pattern/src/chapter12/factorymethod/ThroughputScheduler.java
@@ -1,6 +1,19 @@
 package chapter12.factorymethod;
 
 public class ThroughputScheduler implements ElevatorScheduler {
+
+    private static ElevatorScheduler scheduler;
+
+    private ThroughputScheduler() {}
+
+    public static ElevatorScheduler getInstance() {
+        if (scheduler == null) {
+            scheduler = new ThroughputScheduler();
+        }
+
+        return scheduler;
+    }
+
     @Override
     public int selectElevator(ElevatorManager manager, int destination, Direction direction) {
         return 0;
